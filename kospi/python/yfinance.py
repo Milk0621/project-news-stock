bf4dbeb36850e3609ce270d7eb3f5371cc8a8e26
Lee <Lee@example.com>
2025-04-14 18:15:07 +0900

    qwe

---
+++ kospi/python/yfinance.py
@@ -0,0 +1,24 @@
+import yfinance as yf
+import pandas as pd
+import mplfinance as mpf
+
+ticker = "^KS11"
+cospi = yf.download(ticker, start="2023-01-01", end="2024-01-01", multi_level_index=False)
+#cospi.columns = cospi.columns.get_level_values(0)
+
+#print(cospi.info())
+
+#print(type(cospi.index))
+
+#print(cospi.columns)
+
+#index
+#cospi.index = pd.to_datetime(cospi.index)
+
+# cospi["date"] = cospi.index
+
+#cospi = cospi.reset_index()
+
+print(cospi)
+
+mpf.plot(cospi)
