b5f69a494e2e41107bcfd336da69795a1f4c81e7
kjs <kim@example.com>
2025-04-15 15:29:41 +0900

    뉴스 스케줄러

--- kospi/python/rss.py
+++ kospi/python/rss.py
@@ -3,12 +3,15 @@ import requests
 import pandas as pd
 import schedule
 import time
+from datetime import date
+from datetime import datetime
 
 df = pd.DataFrame()
 df["이름"] = ["네이버경제", "연합뉴스", "SBS경제", "매일경제", "JTBC경제"]
-df["주소"] = ["https://rss.etnews.com/Section902.xml", "https://www.yna.co.kr/rss/economy.xml", "https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=02&plink=RSSREADER", "https://www.mk.co.kr/rss/30100041/", "https://news-ex.jtbc.co.kr/v1/get/rss/section/"]
+df["주소"] = ["https://rss.etnews.com/Section902.xml", "https://www.yna.co.kr/rss/economy.xml", "https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=02&plink=RSSREADER", "https://www.mk.co.kr/rss/30100041/", "https://news-ex.jtbc.co.kr/v1/get/rss/section/20"]
 names = df["이름"]
 urls = df["주소"]
+# print(date.today())
 
 #스케줄러
 def news():
@@ -24,31 +27,30 @@ def news():
         # print(soup)
 
         for item in soup.select('item'):
+
             title = item.title.text
             link = item.link.text
-            date = item.pubDate.text
             content = item.description.text
             data = {
                 "name" : name,
                 "title" : title,
                 "link" : link,
-                "date" : date,
+                "date" : date.today(),
                 "content" : content
             }
             news_data.append(data)
 
     df2 = pd.DataFrame(news_data)
-    df2.to_csv("./rss.csv", index=False)
+    df2.to_csv(f"./{date.today()}.csv", index=False)
     print("저장!")
 
-    #감성 분석
-    #sql 작성
+    #sql 작성!
 
 schedule.every(10).minutes.do(news) #10분마다
-# job = schedule.every(10).minutes.do(news)
 
 while True:
-    # 자정이 되면 스케줄러 취소
-    # job.cancle()
+    if datetime.today().hour == 00 or datetime.today().hour == 0:
+        break
+
     schedule.run_pending()
     time.sleep(2)
