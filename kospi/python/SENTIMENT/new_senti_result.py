37939b43740b84d94b7a98047f75fe6ec6f70c8b
Lee <Lee@example.com>
2025-04-24 18:23:56 +0900

    new_senti_result.py 모두 주석

--- kospi/python/SENTIMENT/new_senti_result.py
+++ kospi/python/SENTIMENT/new_senti_result.py
@@ -20,20 +20,21 @@ day_news = pd.read_csv("./datas/final/day_news_result.csv")
 # day_group_avg.to_csv("./datas/final/senti_result.csv")
 
 
+
 #뉴스상세페이지에 LSTM 분석 확률 붙이기
 
-result = []
-for idx, ser in day_news.iterrows():
-    max = ser[["0","1","2"]].idxmax()
-    if max == "0":
-        max = "부정"
-    elif max == "1":
-        max = "중립"
-    else:
-        max = "긍정"
-    result.append(max)
+# result = []
+# for idx, ser in day_news.iterrows():
+#     max = ser[["0","1","2"]].idxmax()
+#     if max == "0":
+#         max = "부정"
+#     elif max == "1":
+#         max = "중립"
+#     else:
+#         max = "긍정"
+#     result.append(max)
 
 
-news_data = pd.read_csv("./news_data.csv")
-news_data["kor_result"] = result
-news_data.to_csv("./datas/pre/pre_news.csv")
+# news_data = pd.read_csv("./news_data.csv")
+# news_data["kor_result"] = result
+# news_data.to_csv("./datas/pre/pre_news.csv")
