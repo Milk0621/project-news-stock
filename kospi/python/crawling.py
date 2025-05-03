bb8029bc5624847299d5492dde2641d3c9d82c53
kjs <kim@example.com>
2025-05-03 13:01:03 +0900

    크롤링 수정

--- kospi/python/crawling.py
+++ kospi/python/crawling.py
@@ -23,18 +23,19 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), opti
 
 driver.get(f"https://search.naver.com/search.naver?where=news&query=%EC%BD%94%EC%8A%A4%ED%94%BC&sm=tab_opt&sort=0&photo=0&field=0&pd=3&ds=2025.02.01&de=2025.02.01&docid=&related=0&mynews=1&office_type=1&office_section_code=3&news_office_checked=1018&nso=so%3Ar%2Cp%3Afrom20250201to20250415&is_sug_officeid=0&office_category=0&service_area=0")
 
-dates = datetime.datetime(2024, 10, 1)
+dates = datetime.datetime(2025, 4, 21)
 # end = datetime.datetime(2025, 1, 31)
-# dates = dates + datetime.timedelta(days=122) 
+# dates = dates + datetime.timedelta(days=11) 
 
 print(dates) #4월 17일
 
 #언론사, 타이틀, 링크, 날짜, 본문, 이미지
 #이데일리, 아시아경제, 매일경제, 한국경제, 머니투데이
-news_id = ["1018", "1277", "1009", "1015", "1008"]
+#1018, 1277, 1009, 1015, 1008
+news_id = ["1009", "1015", "1008"]
 
 news_data=[]
-for i in range(0, 122):
+for i in range(0, 11):
     month = dates.strftime("%m")
     day = dates.strftime("%d")
     for id in news_id:
@@ -47,7 +48,7 @@ for i in range(0, 122):
         try:
             while True:
                 #Ermefm6A3ilpd9Zvt0OZ
-                boxs = driver.find_elements(By.CSS_SELECTOR, "div.Ermefm6A3ilpd9Zvt0OZ")
+                boxs = driver.find_elements(By.XPATH, '//*[@data-block-id="news/prs_template_v2_news_tab_desk.ts"]/div/div/div/div')
                 driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
                 time.sleep(2)
                 new_height = driver.execute_script("return document.body.scrollHeight")
@@ -57,11 +58,11 @@ for i in range(0, 122):
         except:
             continue
         
-        for i, _ in enumerate(boxs):
+        for i, box in enumerate(boxs):
             con_text= None
             time.sleep(2)
             #bynlPWBHumGsbotLYK9A jT1DuARpwIlNAFMacxlu
-            link = boxs[i].find_element(By.CLASS_NAME, "jT1DuARpwIlNAFMacxlu").get_attribute("href")
+            link = box.find_elements(By.XPATH, "./div")[1].find_element(By.TAG_NAME, "a").get_attribute("href")
             time.sleep(2)
             driver.execute_script(f"window.open('{link}', '_blank')")
             time.sleep(1)
@@ -74,6 +75,35 @@ for i in range(0, 122):
             #제목
             title = driver.title
             try:
+
+                #날짜(시간)
+                try:
+                    date = driver.find_element(By.CSS_SELECTOR, ".dates ul li p:first-child").text
+                    date = date.replace("등록", "")
+                    if "오후" in date:
+                        period = "PM"
+                        date = date.replace("오후", "")
+                    elif "오전" in date:
+                        period = "AM"
+                        date = date.replace("오전", "")
+                    date = date.strip()
+                    date = f"{date} {period}"
+                    print(date)
+                except :
+                    print("이데일리 날짜 패스")
+                    pass
+
+                try:
+                    date = driver.find_element(By.CSS_SELECTOR, ".txt-date").text
+                except:
+                    print("한국경제 날짜 패스")
+
+                try:
+                    date = driver.find_element(By.CSS_SELECTOR, ".date>time").text
+                except:
+                    print("머니투데이")
+                    pass
+
                 #본문
                 #이데일리 : news_body, 한국경제 : article-body, 머니투데이 : textBody
                 content = driver.find_element(By.XPATH, "//*[@class='news_body' or @class='article-body' or @id='textBody']")
@@ -96,6 +126,21 @@ for i in range(0, 122):
                 if con_text:
                     print("던져!")
                     raise
+
+                #날짜(시간)
+                try:
+                    date = driver.find_element(By.CSS_SELECTOR, ".date_box p").text
+                    date = date.replace("입력", "")
+                    print(date)
+                except:
+                    print("아시아경제 날짜 패스")
+                    pass
+
+                try:
+                    date = driver.find_element(By.CSS_SELECTOR, ".time_area dl dd").text
+                except:
+                    print("매일경제 날짜 패스")
+
                 #본문
                 #아시아경제 : article, article_view , 매일경제 : news_cnt_detail_wrap
                 content_box = driver.find_element(By.XPATH, "//*[@class='article_view' or @class='article' or @class='news_cnt_detail_wrap']")
@@ -120,22 +165,28 @@ for i in range(0, 122):
 
             driver.close()
             driver.switch_to.window(driver.window_handles[0])
-            
-            name = boxs[i].find_element(By.CSS_SELECTOR, ".sds-comps-text-type-body2").text
+            date = date.replace(".", "-")
+            secondExists = date.split(":")
+            if len(secondExists) <= 2 :
+                date += ":00"
+            try:
+                dt = datetime.datetime.strptime(date, "%Y-%m-%d %I:%M:%S %p")
+            except:
+                dt = datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
 
+            name = box.find_element(By.CSS_SELECTOR, ".sds-comps-text-type-body2").text
             dict = {
                 "name" : name,
                 "title" : title,
                 "link" : link,
                 "content" : con_text,
                 "img" : img,
-                "date" : dates
+                "date" : dt
             }
             news_data.append(dict)
             print(f"{id}의 {i}번째 저장!")
             
     dates = dates + datetime.timedelta(days=1)
-
 
 df = pd.DataFrame(news_data)
 df.to_csv("./news_data2.csv", index=False)
