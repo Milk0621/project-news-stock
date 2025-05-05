cd54d5187a295663a4b027d69d3792da16e1cbdc
김지선 <wltjs5360@gmail.com>
2025-05-05 13:21:06 +0900

    과거 뉴스 분석 코드

--- kospi/python/SENTIMENT/old_senti_result.py
+++ kospi/python/SENTIMENT/old_senti_result.py
@@ -1,56 +1,38 @@
 import pandas as pd
-import re
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from ML.kobert_finance import kobert_keyword
+import pymysql
 
-#크롤링한 데이터
-keywords = pd.read_csv("./datas/final/keywords.csv")
+df = pd.read_csv("news_data2.csv")
 
-date = keywords["date"].drop_duplicates()
+content = df["content"]
+print(content)
 
-result= []
-for i in date:
-    date_word = keywords[keywords["date"] == i]
-    
-    #날짜별 전체 개수
-    total_cnt = date_word["word_type"].count()
-    
-    #타입 그룹화
-    type = date_word.groupby("word_type").agg('count').reset_index()
-    print(type)
-    bad = type[type["word_type"]==0]
-    mid = type[type["word_type"]==1]
-    good = type[type["word_type"]==2]
-    print(good["keyword"])
-    # print(good["keyword"].values)
-    #감정분석 결과 (긍정, 부정, 중립)
-    good_per = (good["keyword"].values/total_cnt)*100 
-    # print(good_per["keyword"].values)
-    mid_per = (mid["keyword"].values/total_cnt)*100 
-    bad_per = (bad["keyword"].values/total_cnt)*100 
-    print(good_per)
-    if good_per > bad_per:
-        dict = {
-            "date" : i,
-            "result" : "긍정",
-            "good" : good_per,
-            "mid" : mid_per,
-            "bad" : bad_per
-        }
-    elif good_per < bad_per:
-        dict = {
-            "date" : i,
-            "result" : "부정",
-            "good" : good_per,
-            "mid" : mid_per,
-            "bad" : bad_per
-        }
-    else:
-        dict = {
-            "date" : i,
-            "result" : "중립",
-            "good" : good_per,
-            "mid" : mid_per,
-            "bad" : bad_per
-        }
-    result.append(dict)
-df = pd.DataFrame(result)
-df.to_csv("./datas/final/senti_result.csv", index=False)
+conn = pymysql.connect(
+    host="158.247.211.92",
+    user="milk",
+    password="0621",
+    database="kospi"
+    )
+cursor = conn.cursor()
+
+for i, _ in df:
+    senti_result, keywords, percentages = kobert_keyword(content)
+
+    last_row_id = [i+1] * len(keywords)
+    print(last_row_id)
+    print(keywords) 
+    data = list(zip(i+1, keywords))
+
+    #뉴스가 인서트됨
+    #키워드 테이블에 인서트
+    keyword_insert = "insert into keyword(no, keyword) values(%s, %s)"
+    cursor.executemany(keyword_insert,(data))
+    conn.commit()
+
+    bad, mid, good = percentages
+    senti_insert = "insert into senti_result(no, bad, mid, good, result) values(%s, %s, %s, %s, %s)"
+    cursor.execute(senti_insert, (i+1, bad, mid, good, senti_result))
+    conn.commit()
